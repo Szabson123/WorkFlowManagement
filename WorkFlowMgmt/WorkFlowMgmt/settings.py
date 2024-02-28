90b09835b5ee6bafd02afc42c8b1f7e7f285d30c
Szymon Żaba <szymonzaba18@gmail.com>
2024-02-29 00:41:55 +0100

    change sql to postgre/base Profile model

--- WorkFlowMgmt/WorkFlowMgmt/settings.py
+++ WorkFlowMgmt/WorkFlowMgmt/settings.py
@@ -82,8 +82,11 @@ WSGI_APPLICATION = 'WorkFlowMgmt.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'WorkFlow',
+        'USER': 'postgres',
+        'PASSWORD': 'Kaktusik123',
+        'HOST': 'localhost',
     }
 }
 
--- WorkFlowMgmt/base/models.py
+++ WorkFlowMgmt/base/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    email = models.EmailField(null=True, blank=True)
+    number = models.DecimalField(decimal_places=0, max_digits=11, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.user}'
