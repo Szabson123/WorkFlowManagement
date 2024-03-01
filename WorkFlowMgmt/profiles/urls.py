50e2c4e2a922f34ea29127d5ad829f943242dee9
Szabson123 <szymonzaba18@gmail.com>
2024-03-01 13:35:56 +0100

    Added Workers List

--- WorkFlowMgmt/profiles/urls.py
+++ WorkFlowMgmt/profiles/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from profiles.views import ProfileView, main_page
+from profiles.views import ProfileView, ProfileListView, main_page
 
 
 app_name = 'profiles'
@@ -7,5 +7,6 @@ app_name = 'profiles'
 urlpatterns=[
     path('main_page/', main_page, name='main_page'),
     path('profile/', ProfileView.as_view(), name='profile'),
+    path('profiles_list/', ProfileListView.as_view(), name='profiles_list')
 ]
 
--- WorkFlowMgmt/profiles/views.py
+++ WorkFlowMgmt/profiles/views.py
@@ -1,6 +1,7 @@
+from typing import Any
 from django.db.models.query import QuerySet
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
 
 from base.models import Profile
 
@@ -15,3 +16,17 @@ class ProfileView(DetailView):
     
     def get_object(self,):
         return get_object_or_404(Profile, user=self.request.user)
+    
+    
+class ProfileListView(ListView):
+    model = Profile
+    template_name = 'profiles/profiles_list.html'
+    context_object_name = 'profiles'
+    
+    def get_queryset(self):
+        return Profile.objects.all()
+    
+    def get_context_data(self, **kwargs):
+        return super().get_context_data(**kwargs)
+    
+    
