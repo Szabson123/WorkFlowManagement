8575eb770480c7e5c13055f73ad4ad869c96f56d
Szymon Żaba <szymonzaba18@gmail.com>
2024-03-08 11:32:03 +0100

    user searching

--- WorkFlowMgmt/profiles/views.py
+++ WorkFlowMgmt/profiles/views.py
@@ -1,7 +1,6 @@
-from typing import Any
-from django.db.models.query import QuerySet
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView, ListView
+from django.db.models import Q
 
 from base.models import Profile
 
@@ -24,9 +23,19 @@ class ProfileListView(ListView):
     context_object_name = 'profiles'
     
     def get_queryset(self):
-        return Profile.objects.all()
-    
+        queryset = super().get_queryset()
+        search_query = self.request.GET.get('search', None)
+        if search_query:
+            queryset = queryset.filter(
+                Q(user__first_name__iscontains=search_query) |
+                Q(user__last_name__iscontains=search_query)
+            )
+        return queryset
+
     def get_context_data(self, **kwargs):
-        return super().get_context_data(**kwargs)
+        context = super().get_context_data(**kwargs)
+        context['profile_groups'] = [(profile, profile.user.groups.all()) for profile in context['profiles']]
+        profiles = context['profiles']
+        return context
     
     
