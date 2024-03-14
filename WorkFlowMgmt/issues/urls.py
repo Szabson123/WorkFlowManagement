9c13ab20634331654eb57bc36aebb8554112071d
Szabson123 <szymonzaba18@gmail.com>
2024-03-14 11:07:51 +0100

    create, views, urls to issues

--- WorkFlowMgmt/issues/urls.py
+++ WorkFlowMgmt/issues/urls.py
@@ -4,5 +4,7 @@ from issues.views import *
 app_name = 'issues'
 
 urlpatterns = [
-    path('', )
+    path('', IssuesListView.as_view(), name='issue_list'),
+    path('create_issue/', IssueCreateView.as_view(), name='create_issue'),
+    path('detail_issue/<int:pk>/', IssueDetailView.as_view(), name='issue_detail')
 ]
--- WorkFlowMgmt/issues/views.py
+++ WorkFlowMgmt/issues/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from typing import Any
+from django.db.models.base import Model as Model
+from django.db.models.query import QuerySet
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView, CreateView
+from datetime import timezone
 
-# Create your views here.
+from base.models import Issue
+from base.forms import IssueForm
+
+
+class IssuesListView(ListView):
+    template_name = 'issues/issue_list.html'
+    model = Issue
+    context_object_name = 'issues'
+    
+    def get_queryset(self):
+        return Issue.objects.all()
+
+
+class IssueCreateView(CreateView):
+    template_name = 'issues/create_issue.html'
+    model = Issue
+    form_class = IssueForm
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        form.instance.upload_date = timezone.now()
+        
+        return super().form_valid(form)
+
+
+class IssueDetailView(DetailView):
+    template_name = 'issues/issue_detail.html'
+    model = Issue
+    context_object_name = 'issue'
+    
+    def get_object(self, *args, **kwargs):
+        pk = self.kwargs.get('pk')
+        issue = get_object_or_404(Issue, pk=pk)
+        return issue
