6fc4985fd2d689c606b1b2e651988ee542d7adfe
Szabson123 <szymonzaba18@gmail.com>
2024-03-28 15:06:04 +0100

    post details working

--- WorkFlowMgmt/forum/urls.py
+++ WorkFlowMgmt/forum/urls.py
@@ -5,5 +5,7 @@ app_name = 'forum'
 
 urlpatterns = [
     path('', MainForumPage.as_view(), name='forum'),
-    path('new_post/', CreatePostView.as_view(), name='new_post')
+    path('new_post/', CreatePostView.as_view(), name='new_post'),
+    path('my_posts/', MyPostsListView.as_view(), name='my_posts'),
+    path('post_detail/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
 ]
--- WorkFlowMgmt/forum/views.py
+++ WorkFlowMgmt/forum/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
-from django.views.generic import ListView, CreateView
+from django.db.models.base import Model as Model
+from django.db.models.query import QuerySet
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, CreateView, DetailView
 from django.urls import reverse, reverse_lazy
+from django.utils import timezone
 
 from base.models import Forum, Comments
 from base.forms import ForumForm
@@ -14,7 +17,16 @@ class MainForumPage(ListView):
     def get_queryset(self): 
         return Forum.objects.all()
     
+
+class MyPostsListView(ListView):
+    model = Forum
+    context_object_name = 'posts'
+    template_name = 'forum/my_posts.html'
     
+    def get_queryset(self):
+        return Forum.objects.filter(author=self.request.user)
+
+
 class CreatePostView(CreateView):
     template_name = 'forum/new_post.html'
     model = Forum
@@ -22,4 +34,17 @@ class CreatePostView(CreateView):
     success_url = reverse_lazy('forum:forum')
 
     def form_valid(self, form):
+        form.instance.author = self.request.user
+        form.instance.upload_date = timezone.now()
         return super().form_valid(form)
+
+
+class PostDetailView(DetailView):
+    template_name = 'forum/post_detail.html'
+    model = Forum
+    context_object_name = 'post'
+    
+    def get_object(self, *args, **kwargs):
+        pk = self.kwargs.get('pk')
+        post = get_object_or_404(Forum, pk=pk)
+        return post
